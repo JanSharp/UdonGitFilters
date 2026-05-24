094cd247ad7f2bdf7dc121f57ae4045fa271d7c1
JanSharp <steinmeister36@gmail.com>
2026-05-24 18:06:56 +0200

    Split PeekStream into its own file
    Update license year

--- src/Program.cs
+++ src/Program.cs
@@ -4,66 +4,6 @@ using System.Text.RegularExpressions;
 
 namespace UdonGitFilters
 {
-    public class PeekStream(Stream baseStream) : Stream
-    {
-        public Stream underlyingStream = baseStream;
-        private readonly List<byte> bufferedBytes = [];
-        private bool reachedEnd;
-
-        public override bool CanRead => true;
-        public override bool CanSeek => false;
-        public override bool CanWrite => false;
-        public override long Length => throw new NotImplementedException();
-        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public int Peek(byte[]? buffer, int count)
-        {
-            if (buffer != null)
-                bufferedBytes.CopyTo(0, buffer, 0, Math.Min(count, bufferedBytes.Count));
-            if (bufferedBytes.Count >= count)
-                return count;
-            if (reachedEnd)
-                return bufferedBytes.Count;
-            int GetClampedMissingByteCount() => Math.Min(1024 * 1024, count - bufferedBytes.Count);
-            byte[] secondaryBuffer = new byte[GetClampedMissingByteCount()];
-            while (bufferedBytes.Count < count)
-            {
-                int countReadIntoBuffer = underlyingStream.Read(secondaryBuffer, 0, GetClampedMissingByteCount());
-                if (countReadIntoBuffer <= 0)
-                {
-                    reachedEnd = true;
-                    break;
-                }
-                if (buffer != null)
-                    Buffer.BlockCopy(secondaryBuffer, 0, buffer, bufferedBytes.Count, countReadIntoBuffer);
-                for (int i = 0; i < countReadIntoBuffer; i++)
-                    bufferedBytes.Add(secondaryBuffer[i]);
-            }
-            return bufferedBytes.Count;
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            if (bufferedBytes.Count == 0)
-                return underlyingStream.Read(buffer, offset, count);
-            int bufferedCount = Math.Min(count, bufferedBytes.Count);
-            bufferedBytes.CopyTo(0, buffer, offset, bufferedCount);
-            bufferedBytes.RemoveRange(0, bufferedCount);
-            return bufferedCount;
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            underlyingStream.Dispose();
-            base.Dispose(disposing);
-        }
-
-        public override void Flush() => throw new NotImplementedException();
-        public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
-        public override void SetLength(long value) => throw new NotImplementedException();
-        public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
-    }
-
     public class Program
     {
         private const int BufferSize = 1024 * 1024;
